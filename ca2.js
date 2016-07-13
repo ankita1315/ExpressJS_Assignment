24b2295c0502a4b89e45e30f8b33ed860bcf543e
ankita1315 <ankitakulkarni370@gmail.com>
2016-07-13 04:51:20 +0000

    initial commit

---
+++ ca2.js
@@ -0,0 +1,17 @@
+module.exports={
+               "add": function(a,b){
+                 return (a+b);
+               },
+
+               "sub": function(a,b){
+                 return (a-b);
+               },
+
+               "multi": function(a,b){
+                 return (a*b);
+               },
+
+               "divide": function(a,b){
+                 return (a/b);
+               }
+           }
